577e95dca3052cf13ac0bbe5ff1b6a217386a591
hsyemfk <103497721+hsyemfk@users.noreply.github.com>
2022-08-23 21:53:14 +0430

    Add files via upload

---
+++ Config.py
@@ -0,0 +1,21 @@
+auth = "xffmaicbkrrlofhuysquscwmenjpriai"
+guidgap = "g0BbWVQ04efc221bc358b350870b9808"
+admins = ["u0DwHdd07c161b00d5631317aff670dd","u0Ds5ni0168441041bad4f1bd9e78cbe"]
+
+
+
+alertadmin = """
+شما ادمین شدید
+"""
+alertwelcome = """سلام خوش اومدید"""
+bazkardangap = """
+گروه باز شد و فلان
+"""
+alertremove = """
+مثلا کاربر ریم شد و فلان
+"""
+alertstart = """ربات فعال """
+alertstop = """
+ربات خاموش شد
+"""
+helpbotnull = """i"""
